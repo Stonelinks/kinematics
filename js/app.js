f647f5416349a8493162e4665436628b3c47cd58
Lucas Doyle <ldoyle@airware.com>
2015-12-31 01:32:51 +0000

    slider

--- js/app.js
+++ js/app.js
@@ -17,6 +17,8 @@ App.addRegions({
     content: '#content'
 });
 
+var EPSILON = 0.0000000000001
+
 var COLORS = {
     white: "#FFFFFF",
     red: "#FF0000",
@@ -50,10 +52,18 @@ class Link {
         context.stroke()
     }
 
+    onAngleChange(cb) {
+        this._onAngleChange = cb
+    }
+
     pointAt(x, y) {
+        var lastAngle = this.angle
         var dx = x - this.x;
         var dy = y - this.y;
         this.angle = Math.atan2(dy, dx)
+        if (this._onAngleChange && Math.abs(this.angle - lastAngle) > EPSILON) {
+            this._onAngleChange.call(this, this.angle)
+        }
     }
 
     resetBase(x, y) {
@@ -85,8 +95,7 @@ class Cursor {
         }
     }
 
-    onMouseUp(mousePos) {
-        this.onMouseMove(mousePos)
+    onMouseUp() {
         this.state = this.state == CURSOR_STATES.locked ? CURSOR_STATES.unlocked : CURSOR_STATES.locked
     }
 
@@ -146,107 +155,215 @@ class ArticulatedSystem {
     }
 }
 
+var SliderView = Marionette.ItemView.extend({
+    template: require('../tmpl/slider.hbs'),
+
+    min: 0,
+    max: 180,
+    step: 1,
+
+    templateHelpers: function () {
+        return {
+            min: this.getOption('min'),
+            max: this.getOption('max'),
+            step: this.getOption('step')
+        };
+    },
+
+    events: {
+        'change input': 'onUIChange',
+        'mousedown input': 'startPolling',
+        'mouseup input': 'stopPolling',
+        'touchstart input': 'startPolling',
+        'touchend input': 'stopPolling'
+    },
+
+    value: null,
+    onUIChange: function () {
+        this.value = parseFloat(this.$el.find('input').val());
+        this.$el.find('small').text(this.value)
+    },
+
+    setValue: function (value) {
+        this.value = parseFloat(value);
+        this.$el.find('input').val(this.value);
+        this.$el.find('small').text(this.value)
+    },
+
+    _pollingInterval: null,
+    startPolling: function () {
+        this._pollingInterval = setInterval(this.onUIChange.bind(this), 100)
+    },
+
+    stopPolling: function () {
+        clearInterval(this._pollingInterval)
+    },
+
+    onShow: function () {
+        this.onUIChange()
+    },
+
+    onDestroy: function () {
+        this.stopPolling()
+    }
+});
+
+var RowView = Marionette.LayoutView.extend({
+    template: require('../tmpl/row.hbs'),
+
+    childViews: [],
+
+    initialize: function () {
+        this.childViews = this.getOption('childViews')
+        this.childViews.forEach(function (View, index) {
+            this.addRegion('row' + index, '.row:nth-of-type(' + (index + 1) + ')');
+        }.bind(this));
+    },
+
+    templateHelpers: function () {
+        return {
+            childViews: this.childViews
+        };
+    },
+
+    onShow: function () {
+        this.childViews.forEach(function (View, index) {
+            this.getRegion('row' + index).show(View);
+        }.bind(this));
+    }
+});
+
+var CanvasView = Marionette.ItemView.extend({
+    template: require('../tmpl/canvas.hbs'),
+
+    events: {
+        'mousemove': 'onMouseMove',
+        'mouseup': 'onMouseUp'
+    },
+
+    width: null,
+    height: null,
+
+    updateWidthAndHeight: function () {
+        var rect = this.$el.find('canvas')[0].getBoundingClientRect();
+        this.width = rect.width;
+        this.height = rect.height
+    },
+
+    onWindowResize: function () {
+        this.updateWidthAndHeight();
+        this.canvas.width = this.context.width = this.width;
+        this.canvas.height = this.context.height = this.height;
+        if (this.system) {
+            this.system.x = this.width / 2;
+            this.system.y = this.height / 2;
+        }
+    },
+
+    initialize: function () {
+        this.system = this.getOption('system')
+        this.cursor = this.getOption('cursor')
+        this.onWindowResize = this.onWindowResize.bind(this);
+        this.animate = this.animate.bind(this)
+    },
+
+    getMousePos: function (evt) {
+        var rect = this.$el.find('canvas')[0].getBoundingClientRect();
+        var root = document.documentElement;
+
+        // return relative mouse position
+        var mouseX = evt.clientX - rect.left - root.scrollLeft;
+        var mouseY = evt.clientY - rect.top - root.scrollTop;
+        return {
+            x: mouseX,
+            y: mouseY
+        };
+    },
+
+    mousePos: {
+        x: 0,
+        y: 0
+    },
+
+    onMouseMove: function (e) {
+        this.mousePos = this.getMousePos(e);
+        this.cursor.onMouseMove(this.mousePos)
+    },
+
+    onMouseUp: function (e) {
+        this.cursor.onMouseUp()
+    },
+
+    onShow: function () {
+        this.canvas = this.$el.find('canvas')[0];
+        this.context = this.canvas.getContext('2d');
+        this.context.fillStyle = "rgb(59,0,86)";
+
+        this.onWindowResize();
+
+        this.animate()
+    },
+
+    onDestroy: function () {
+        window.removeEventListener('resize', this.onWindowResize, false);
+    },
+
+    animate: function () {
+        this.context.clearRect(0, 0, this.width, this.height);
+
+        this.cursor.render(this.context);
+
+        this.system.render(this.context);
+        this.system.solveIK(this.cursor.x, this.cursor.y)
+
+        requestAnimationFrame(this.animate);
+        //setTimeout(this.animate, 1000)
+    }
+});
+
+var JointSliderView = SliderView.extend({
+    min: -Math.PI,
+    max: Math.PI,
+    step: .001,
+
+    initialize: function () {
+        this.index = this.getOption('index')
+        this.system = this.getOption('system')
+
+        this.system.links[this.index].onAngleChange(function (value) {
+            if (this.index == 0) {
+                console.log(value)
+            }
+            this.setValue(value)
+        }.bind(this))
+    }
+});
 
 App.module("Kinematics", function (Mod, App, Backbone, Marionette, $, _) {
 
-    var CanvasView = Marionette.ItemView.extend({
-        template: require('../tmpl/canvas.hbs'),
+    var cursor = new Cursor(0, 0);
+    var system = new ArticulatedSystem(this.width / 2, this.height / 2);
 
-        events: {
-            'mousemove': 'onMouseMove',
-            'mouseup': 'onMouseUp'
-        },
+    system.addLink(100)
+    system.addLink(100)
+    system.addLink(100)
 
-        width: null,
-        height: null,
+    var childViews = [new CanvasView({
+        cursor: cursor,
+        system: system
+    })]
 
-        updateWidthAndHeight: function () {
-            var rect = this.$el.find('canvas')[0].getBoundingClientRect();
-            this.width = rect.width;
-            this.height = rect.height
-        },
-
-        onWindowResize: function () {
-            this.updateWidthAndHeight();
-            this.canvas.width = this.context.width = this.width;
-            this.canvas.height = this.context.height = this.height;
-            if (this.system) {
-                this.system.x = this.width / 2;
-                this.system.y = this.height / 2;
-            }
-        },
-
-        initialize: function () {
-            this.onWindowResize = this.onWindowResize.bind(this);
-            this.animate = this.animate.bind(this)
-        },
-
-        getMousePos: function (evt) {
-            var rect = this.$el.find('canvas')[0].getBoundingClientRect();
-            var root = document.documentElement;
-
-            // return relative mouse position
-            var mouseX = evt.clientX - rect.left - root.scrollLeft;
-            var mouseY = evt.clientY - rect.top - root.scrollTop;
-            return {
-                x: mouseX,
-                y: mouseY
-            };
-        },
-
-        mousePos: {
-            x: 0,
-            y: 0
-        },
-
-        onMouseMove: function (e) {
-            this.mousePos = this.getMousePos(e);
-            this.cursor.onMouseMove(this.mousePos)
-        },
-
-        onMouseUp: function (e) {
-            this.mousePos = this.getMousePos(e);
-            this.cursor.onMouseUp(this.mousePos)
-        },
-
-        onShow: function () {
-            this.canvas = this.$el.find('canvas')[0];
-            this.context = this.canvas.getContext('2d');
-            this.context.fillStyle = "rgb(59,0,86)";
-
-            this.onWindowResize();
-
-            this.cursor = new Cursor(0, 0);
-
-            this.system = new ArticulatedSystem(this.width / 2, this.height / 2);
-            this.system.addLink(100)
-            this.system.addLink(100)
-            this.system.addLink(100)
-            this.system.addLink(100)
-
-            this.animate()
-        },
-
-        onDestroy: function () {
-            window.removeEventListener('resize', this.onWindowResize, false);
-        },
-
-        animate: function () {
-            this.context.clearRect(0, 0, this.width, this.height);
-
-            this.cursor.render(this.context);
-
-            this.system.render(this.context);
-            this.system.solveIK(this.cursor.x, this.cursor.y)
-
-            requestAnimationFrame(this.animate);
-            //setTimeout(this.animate, 1000)
-        }
-    });
-
+    for (var i = 0; i < system.links.length; i++) {
+        childViews.push(new JointSliderView({
+            system: system,
+            index: i
+        }))
+    }
 
     Mod.addInitializer(function () {
-        App.content.show(new CanvasView());
+        App.content.show(new RowView({
+            childViews: childViews
+        }));
     });
 });
 
